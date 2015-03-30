8f7573aa400be255cb79b5ed231cf681dbbaaa77
Tamas Gal <himself@tamasgal.com>
2015-03-30 12:53:08 +0200

    Initial structure

---
+++ consoleheroes/__init__.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# Filename: __init__.py
+"""
+ConsoleHeroes
+
+"""
+from __future__ import division, absolute_import, print_function
+
+from consoleheroes.__version__ import version, version_info
+
+__author__ = "Jonas Reubelt, Tamas Gal"
+__credits__ = []
+__license__ = "MIT"
+__version__ = version
+__status__ = "Development"
